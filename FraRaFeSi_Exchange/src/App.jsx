246914a5a9697e0a3cbfeb387bc8b7fc8f5aa71f
Palmieri Federico <palmierifederico3@gmail.com>
2024-05-14 21:35:51 +0200

    feat: created WalletCard

--- FraRaFeSi_Exchange/src/App.jsx
+++ FraRaFeSi_Exchange/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Splash from "./components/Splash";
+import { WalletCard } from "./components/WalletCard";
 
 function App() {
   return (
---
+++ FraRaFeSi_Exchange/src/components/WalletCard.jsx
@@ -0,0 +1,21 @@
+import pointed from "../assets/pointed.svg";
+import cryptoCoin from "../assets/crypto.svg";
+export function WalletCard() {
+  return (
+    <div className="m-2 rounded-lg p-4 bg-gradient-to-r from-bluelight via-violet text-whiteText">
+      <div className="flex justify-between mb-2">
+        <p className="text-xs ">Multi-Coin Wallet</p>
+        <button className="max-w-6 ">
+          <img src={pointed} />
+        </button>
+      </div>
+      <div className="flex justify-start mb-8">
+        <h1 className="text-3xl font-bold ">$ 123.45</h1>
+      </div>
+      <div className="flex justify-between">
+        <p className="text-xs ">fsailfjkshlfiuahdfsliud</p>
+        <img src={cryptoCoin} className="max-w-6" />
+      </div>
+    </div>
+  );
+}
