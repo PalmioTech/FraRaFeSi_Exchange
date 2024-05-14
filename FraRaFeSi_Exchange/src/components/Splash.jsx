3facb346fa99359a2cac55e4a6225cc22c0d28e0
Palmieri Federico <palmierifederico3@gmail.com>
2024-05-14 19:29:57 +0200

    feat: create SplashPage

---
+++ FraRaFeSi_Exchange/src/components/Splash.jsx
@@ -0,0 +1,22 @@
+import LogoApp from "../assets/logoApp.png";
+export default function Splash() {
+  return (
+    <div className="flex justify-center items-center m-auto">
+      <div className="flex flex-col justify-evenly p-4">
+        <div>
+          <img className="w-full max-w-xs" src={LogoApp} />
+        </div>
+
+        <div className="flex flex-col items-center gap-3 text-whiteText">
+          <h1 className="text-4xl font-bold text-center">FraRaFeSi</h1>
+          <div className="flex flex-col gap-10">
+            <p className="text-center text-2xl">Exchange</p>
+            <button className="w-full rounded-full bg-blue-500 px-6 py-2 font-bold text-grayBG">
+              Entra
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- FraRaFeSi_Exchange/tailwind.config.js
+++ FraRaFeSi_Exchange/tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      grayBG: "#1b1b35",
+      bluelight: "#27c4ff",
+      greenwater: "#27c3ff",
+      violet: "#653ffe",
+      green: "#25b461",
+      redlight: "#eb4747",
+      whiteText: "#e4eeff",
+    },
   },
   plugins: [],
 };
