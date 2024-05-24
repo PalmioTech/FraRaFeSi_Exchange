ffbf18c7f053ce5b01beddefe8a5515f1a09436a
Palmieri Federico <palmierifederico3@gmail.com>
2024-05-24 19:31:24 +0200

    feat: funzionalità accesso

--- FraRaFeSi_Exchange/src/App.jsx
+++ FraRaFeSi_Exchange/src/App.jsx
@@ -4,10 +4,15 @@ import Splash from "./components/Splash";
 import AppContent from "./components/AppContent";
 function App() {
   const [page, setPage] = useState("splash");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <div className="min-h-screen bg-blackText max-w-xl mx-auto">
-      {page === "walletpage" ? (
-        <Splash setPageHandler={setPage} />
+      {page === "splash" && !isAuthenticated ? (
+        <Splash
+          setIsAuthenticated={setIsAuthenticated}
+          setPageHandler={setPage}
+        />
       ) : (
         <AppContent page={page} setPage={setPage} />
       )}
