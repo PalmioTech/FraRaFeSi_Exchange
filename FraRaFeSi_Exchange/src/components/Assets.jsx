f6da8213cea39210fc5e5a33ff8ff72138feb266
Raffuz <raffaele11596@gmail.com>
2024-05-27 16:16:24 +0200

    API CoinMarketCap

--- FraRaFeSi_Exchange/src/components/Assets.jsx
+++ FraRaFeSi_Exchange/src/components/Assets.jsx
@@ -1,80 +1,71 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Assets() {
   const [data, setData] = useState(null);
-
-  const [searchToken, setSearchToken] = useState("");
+  const [searchTerm, setSearchTerm] = useState(""); // Stato per l'input di ricerca
+  const [error, setError] = useState(null); // Stato per gli errori
 
   useEffect(() => {
-    fetch(`	https://api.coincap.io/v2/assets`, {
-      method: "GET",
-      headers: { Accept: "application/json;version=20230302" },
-    })
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log(responseData);
-        setData(responseData.data);
+    axios
+      .get("/api/cryptocurrency/listings/latest")
+      .then((response) => {
+        console.log("API Response:", response.data);
+        setData(response.data.data);
       })
-      .catch((error) => console.error("Error fetching data:", error));
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      });
   }, []);
 
+  // Funzione per gestire il cambiamento dell'input di ricerca
   const handleSearchChange = (event) => {
-    setSearchToken(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
-  const filteredData = data
-    ? data.filter((token) =>
-        token.name.toLowerCase().includes(searchToken.toLowerCase())
-      )
-    : [];
+  // Filtra i dati in base al valore dell'input di ricerca
+  const filteredData = data?.filter(
+    (token) =>
+      token.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      token.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="max-h-screen flex items-center justify-center px-3 py-5 ">
-      <div className="w-full rounded-xl  relative shadow-lg shadow-violet">
-        <h1 className="text-2xl p-2   mb-2 ml-3 mt-3 text-whiteText">
-          Crypto Market
-        </h1>
-        <div className="w-full p-4 text-white ">
+    <div className="max-h-screen flex items-center justify-center px-3 py-5">
+      <div className="w-full rounded-3xl relative">
+        <h1 className="text-2xl p-2 underline mb-2 text-whiteText">Assets</h1>
+        <div className="w-full p-4 text-white">
           <input
             type="text"
-            className="w-full rounded-lg bg-transparent outline-violet shadow-sm shadow-violet p-3 pl-4 text-sm   "
+            className="w-full text-black rounded-full border-2 p-3 pl-4 text-sm shadow-lg shadow-violet"
             placeholder="Search..."
-            value={searchToken}
-            onChange={handleSearchChange}
+            value={searchTerm}
+            onChange={handleSearchChange} // Aggiorna lo stato di ricerca ad ogni cambiamento
           />
         </div>
-        <div className=" px-2 text-whiteText ">
+        <div className="px-2 text-whiteText border-t-violet shadow-xl shadow-violet">
+          {error && <p className="text-red-500">{error}</p>}
           <ul>
-            {data &&
-              filteredData.slice(0, 10).map((token, index) => (
+            {filteredData &&
+              filteredData.slice(0, 6).map((token, index) => (
                 <li
                   key={index}
                   className="mb-2 p-3 shadow-lg rounded border-b hover:border-violet flex items-center"
                 >
-                  {/* <div className="w-16 text-3xl mr-3">
+                  <div className="w-16 h-16 text-3xl mr-3">
                     <img
-                      src={token.attributes.image_url}
+                      src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${token.id}.png`}
                       alt={token.name}
-                      className="max-w-8"
+                      className="w-full h-full object-contain"
                     />
-                  </div> */}
-                  <div className="w-full flex justify-between items-center">
-                    <p className="w-16">
-                      {token.name}({token.symbol})
-                    </p>
-                    <p>
-                      {new Intl.NumberFormat("it-IT", {
-                        style: "decimal",
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      }).format(token.priceUsd)}
-                    </p>
-
-                    <p className="text-green">
-                      {parseFloat(token.changePercent24Hr)
-                        .toFixed(2)
-                        .replace(".", ",")}
-                      %
-                    </p>
+                  </div>
+                  <div className="w-full">
+                    {token.name}{" "}
+                    <span className="ml-3 text-gray-400">{token.symbol}</span>
+                  </div>
+                  <div className="text-green">
+                    ${parseFloat(token.quote.USD.price).toFixed(2)}
                   </div>
                 </li>
               ))}
--- FraRaFeSi_Exchange/vite.config.js
+++ FraRaFeSi_Exchange/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://pro-api.coinmarketcap.com/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        headers: {
+          "X-CMC_PRO_API_KEY": "dd296c4e-2278-420e-a39e-660a9e0ea215",
+          Accept: "application/json",
+        },
+      },
+    },
+  },
+});
