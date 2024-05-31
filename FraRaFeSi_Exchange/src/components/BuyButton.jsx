12918802c68e5bedc8bb451b0cd531b27c3f47c7
Palmieri Federico <palmierifederico3@gmail.com>
2024-05-31 18:52:54 +0200

    fix: button Buy

--- FraRaFeSi_Exchange/src/components/BuyButton.jsx
+++ FraRaFeSi_Exchange/src/components/BuyButton.jsx
@@ -16,7 +16,9 @@ export function BuyButton({ setPage }) {
         <button
           onClick={handleClick}
           disabled={!selectedAsset.selectedAsset}
-          className="flex justify-center max-w-10 rounded-full bg-violet w-full py-2 mx-2 mt-3 hover:outline hover:outline-2 hover:outline-greenwater "
+          className={`flex justify-center max-w-10 rounded-full bg-violet w-full py-2 mx-2 mt-3 hover:outline hover:outline-2 hover:outline-greenwater ${
+            !selectedAsset.selectedAsset ? "opacity-50 cursor-not-allowed" : ""
+          }`}
         >
           <BuyButtonIcon />
         </button>
