b6b3d0a6b601003827643c493d607b4c2c5825b0
Palmieri Federico <palmierifederico3@gmail.com>
2024-06-06 13:10:21 +0200

    fix: modificato YourWallet

--- FraRaFeSi_Exchange/src/components/Wallet.jsx
+++ FraRaFeSi_Exchange/src/components/Wallet.jsx
@@ -1,41 +1,45 @@
+import { useSelector } from "react-redux";
+import { useGetUserByEmailQuery } from "../reducers/apiSlice";
+import { useEffect, useState } from "react";
+
 export function Wallet() {
-  const cryptoCurrency = [
-    {
-      name: "Bitcoin",
-      amount: 50,
-    },
-    {
-      name: "Ethereum",
-      amount: 25,
-    },
-    {
-      name: "Ripple",
-      amount: 100,
-    },
-    {
-      name: "Litecoin",
-      amount: 10,
-    },
-  ];
+  const email = useSelector((state) => state.user.data.email);
+  const { data: user, error, isLoading } = useGetUserByEmailQuery(email);
+  const [cryptoCurrency, setCryptoCurrency] = useState([]);
+
+  useEffect(() => {
+    if (user && user.length > 0) {
+      const currentUser = user[0];
+      setCryptoCurrency(currentUser.wallet);
+    }
+  }, [user]);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data: {error.message}</p>;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full items-center justify-center py-5 ">
       <div>
-        <h2 className="mb-3 text-xl font-extrabold text-white p-4">
-          Your Coins:
-        </h2>
+        <h1 className="mb-3 text-3xl font-extrabold text-white">
+          Your{" "}
+          <mark className="px-2 text-white bg-violet rounded">
+            {" "}
+            Crypto Wallet
+          </mark>
+        </h1>
       </div>
-      <div>
-        <ol>
+      <div className=" text-whiteText border-t-violet shadow-xl shadow-violet mt-2 w-11/12">
+        <ul className="max-h-96 overflow-y-auto">
           {cryptoCurrency.map((crypto, index) => (
             <li
               key={index}
-              className="mb-3 text-xl font-extrabold text-white p-4"
+              className="first-letter:mb-2 p-1 shadow-lg rounded border-b hover:border-violet  truncate cursor-pointer flex justify-between px-2 text-xl mt-2 "
             >
-              {crypto.name}: {crypto.amount}
+              <div>{crypto.name}</div>
+              <div>{crypto.name}</div>
+              <div>{crypto.amount}</div>
             </li>
           ))}
-        </ol>
+        </ul>
       </div>
     </div>
   );
